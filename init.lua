6d34ce8121d0437a69ea91332497c68b757ea42c
SharathHuddar <sharath.huddar@finbox.in>
2025-12-25 13:10:30 +0530

    add indent-blankline

--- init.lua
+++ init.lua
@@ -1037,8 +1037,14 @@ require('lazy').setup(
       -- use opts = {} for passing setup options
       -- this is equivalent to setup({}) function
     },
+    {
+      { -- Add indentation guides even on blank lines
+        'lukas-reineke/indent-blankline.nvim',
+        main = 'ibl',
+        opts = {},
+      },
+    },
   },
-
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
