d238e82a9b75e765753e62efb956590df4f613a8
SharathHuddar <sharath.huddar@finbox.in>
2026-01-06 13:45:10 +0530

    add plugin to grep and find within a dir

--- init.lua
+++ init.lua
@@ -339,6 +339,19 @@ require('lazy').setup(
       },
     },
     {
+      'princejoogie/dir-telescope.nvim',
+      dependencies = { 'nvim-telescope/telescope.nvim' },
+      config = function()
+        require('dir-telescope').setup {
+          -- these are the default options set
+          hidden = true,
+          no_ignore = false,
+          show_preview = true,
+          follow_symlinks = false,
+        }
+      end,
+    },
+    {
       'nvim-telescope/telescope-file-browser.nvim',
       event = 'VimEnter',
       dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
@@ -425,6 +438,7 @@ require('lazy').setup(
         vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
         vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
         vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+        vim.keymap.set('n', '<leader>sdg', '<cmd>Telescope dir live_grep<CR>', { noremap = true, silent = true })
         vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
         vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
         vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
