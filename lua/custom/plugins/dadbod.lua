b093c2abd488c0d33b55772c41deeb6c5d0d0045
SharathHuddar <sharath.huddar@finbox.in>
2026-01-14 14:57:07 +0530

    add keymap to toggle dadbod UI

---
+++ lua/custom/plugins/dadbod.lua
@@ -0,0 +1,19 @@
+return {
+  'kristijanhusak/vim-dadbod-ui',
+  dependencies = {
+    { 'tpope/vim-dadbod', lazy = true },
+    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+  },
+  cmd = {
+    'DBUI',
+    'DBUIToggle',
+    'DBUIAddConnection',
+    'DBUIFindBuffer',
+  },
+  init = function()
+    vim.g.db_ui_use_nerd_fonts = 1
+    vim.keymap.set('n', '<leader>td', '<cmd>DBUIToggle<CR>', {
+      desc = 'Toggle Dadbod UI',
+    })
+  end,
+}
